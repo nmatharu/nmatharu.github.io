d3fca54bf56713b1ad6e96b69f54183ed282c2bf
nmatharu <nmatharu338@gmail.com>
2023-03-30 16:20:40 -0700

    Smooth scroll padding

--- js/main.js
+++ js/main.js
@@ -205,6 +205,22 @@
       });
 
   	});  
+
+	$('.smoothscrollpad').on('click', function (e) {
+	 	
+		e.preventDefault();
+
+	  var target = this.hash,
+	   $target = $(target);
+
+	   $('html, body').stop().animate({
+		  'scrollTop': $target.offset().top + 10
+	 }, 800, 'swing', function () {
+		 window.location.hash = target + 10;
+		//  window.location.hash = target;
+	 });
+
+	 });  
   
 
    /*---------------------------------------------------- */
